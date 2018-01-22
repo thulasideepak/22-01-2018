d6bc63393ab78a2035b14317161de90186506ba3
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-01-22 16:01:28 +0530

    Create 2 NUMBERS N&K.C
    nil

---
+++ 2 NUMBERS N&K.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int arr[] ={1,5,7,-1,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int sum=6;
+    printf("Count of pairs is");
+    printf("getPairsCount(arr,n,sum));
+    return 0;
+}
