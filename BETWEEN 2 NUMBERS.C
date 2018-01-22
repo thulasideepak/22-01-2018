c5bc5ca6e1c59fad61b42cf6874b18c15c96383e
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-01-22 16:52:49 +0530

    Create BETWEEN 2 NUMBERS.C
    nil

---
+++ BETWEEN 2 NUMBERS.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int firstNumber,secondNumber,sumOfTwoNumbers;
+  printf("Enter two integers: ");
+  scanf("%d %d",&firstNumber,&secondNumber);
+ sumOfTwoNumbers=firstNumber+secondNumber;
+ Printf("%d+%d=%d",firstNumber,secondNumber,sumOfTwoNumbers);
+ return 0;
+}
